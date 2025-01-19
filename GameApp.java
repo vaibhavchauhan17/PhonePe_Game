26efee33cb9b6b078e09af3ba0c78dbda80e21fd
Vaibhav Kumar Chauhan <69720351+vaibhavchauhan17@users.noreply.github.com>
2025-01-19 17:12:32 +0530

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -3,10 +3,11 @@ package PhonePe;
 public class GameApp {
     public static void main(String[] args) {
         GameService gameService = new GameService();
-        gameService.initGame(8);
+        gameService.initGame(6);
 
-        gameService.addShip("SH1", 3, 0, 0, 4, 4);
-        gameService.addShip("SH2", 2, 1, 1, 5, 5);
+        // gameService.addShip("SH1", 2, 1, 5, 4, 4);
+        gameService.addShip("SH1", 2, 5, 1, 4, 4);
+        // gameService.addShip("SH2", 2, 1, 1, 5, 5);
 
         gameService.displayBattlefields();
         gameService.startGame();
